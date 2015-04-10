4911450bb77dad0d9bb3bfdc8f0148d2bc195436
Chris Dostert <chris.dostert@live.com>
2015-04-10 14:35:41 -0700

    Update PublishAppeaseTaskTemplate.psm1

--- Src/PublishAppeaseTaskTemplate.psm1
+++ Src/PublishAppeaseTaskTemplate.psm1
@@ -1,7 +1,7 @@
 # halt immediately on any errors which occur in this module
 $ErrorActionPreference = 'Stop'
 
-New-Alias -Name Invoke -Value Appease.Client\Publish-AppeaseTaskTemplate
+New-Alias -Name Invoke -Value Appease.Client\Publish-AppeasePowerShellTaskTemplate
 
 Export-ModuleMember -Alias Invoke
 
